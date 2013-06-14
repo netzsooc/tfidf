714f7406fd28ef9beac37aad1308e3856526d30e
netzsooc <netzsooc@gmail.com>
2013-06-14 09:53:35 -0500

    stoplist augmented

--- tfidf/src/tfidf/main.py
+++ tfidf/src/tfidf/main.py
@@ -15,10 +15,12 @@ if __name__ == '__main__':
                   "elsevier", "springer", "berlin", "heidelberg", "published",
                   "springer-verlag", "ieee", "acm", "Â©".lower(), "2013",
                   "2012", "2011", "2010", "press", "authors", "rights",
-                  "reserved", "mexico", "abstract", "copyright", "ltd" "bv",
+                  "reserved", "mexico", "abstract", "copyright", "ltd", "bv",
                   "new", "york", "publishing", "limited", '#', '#17', ")",
-                  "("]
+                  "(", "amp", "gt", "gt10", "gt109", "gt120", "and/or", "1",
+		  "2"]
     sw = stopwords.words("english") + special_sw
+    ignorelist = "!?.,;:_'\"#$&()"
     
     D = dict()
     W = dict()
@@ -27,30 +29,31 @@ if __name__ == '__main__':
         i = 1
         for line in abst:
             if line.strip():
-                temp_line = list()
+#                 temp_line = list()
                 D[i] = []
                 for word in line.strip().split():
-                    word = word.lower().translate(None, "!?.,;:_'\"")
-                    if word not in sw:
-                        if word:
-                            W.setdefault(word, i)
-                            D[i].append(word)
+                    word = word.lower().translate(None, ignorelist)
+                    if word in sw:
+                        continue
+                    if word:
+                        W.setdefault(word, []).append(i)
+                        D[i].append(word)
                 i += 1
 
+    W_ = sorted(W.keys())
     keys_ = sorted(D.keys())
-    terms = sorted(W.keys())
-    print keys_[:10]
-    print terms[:10]
-#     quit()
-    tfidfmatrix = ["Term"] + [k for k in keys_]
-    
-    for w in terms:
-        row = [w]
-        for i in keys_:
-            d = D[i]
-            print w, d
-            row.append(tfidf(w, d, D))
-            
-    
+    tfmatrix = [["Term"] + [k for k in keys_]]
+
+    for term in W_[:10]:
+        temp = [term]
+
+        for k in keys_[:20]:
+	    d = D[k]
+	    tf = d.count(term)
+	    temp.append(tf)
+
+        tfmatrix.append(temp)
+     
+    #print ("W =", len(W), "W =", len(secondW))
     for r in tfidfmatrix[:20]:
-        print r[:10]
+        print r
